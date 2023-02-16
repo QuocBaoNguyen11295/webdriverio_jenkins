f7d7e3ada5daba858f5519cd2d0525867dc72006
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-02-17 01:00:57 +0700

    Run headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -69,7 +69,10 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': { 
+            args: ["--headless", "user-agent=...","--disable-gpu","--window-size=1440,735"]
+       }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
