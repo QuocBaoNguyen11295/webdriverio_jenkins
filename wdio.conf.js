cf0301767428f8d5839305ce46148359f31a6118
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-02-17 13:16:14 +0700

    Spec report

--- wdio.conf.js
+++ wdio.conf.js
@@ -147,7 +147,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: [
+    reporters: ['spec',
         ['junit', {
             outputDir: './report',
             outputFileFormat: function(options){
