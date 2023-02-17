d11e1305f4769cd08a381846e36d7785123077bf
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-02-17 11:56:57 +0700

    add reoprter

--- wdio.conf.js
+++ wdio.conf.js
@@ -147,7 +147,12 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'dot',
+        ['junit', {
+            outputDir: './'
+        }]
+    ],
 
 
     
