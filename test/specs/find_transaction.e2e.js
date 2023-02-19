12c663bf8fd6ea832610ed4fa1d13f8ac04bad85
QuocBaoNguyen11295 <nqb11295@gmail.com>
2023-02-19 14:05:32 +0700

    Update script

--- test/specs/find_transaction.e2e.js
+++ test/specs/find_transaction.e2e.js
@@ -11,7 +11,7 @@ describe('Find Transaction',()=>{
         await browser.open_tab_account_activity('Find Transactions')
         await browser.fill_out_information_for_finding_transaction('Find Transactions','ONLINE TRANSFER REF #UKKSDRQG6L',900,1000,`Deposit`)
         await browser.select_from_date_for_finding_transaction(01,"September 2012")
-        await browser.select_to_date_for_finding_transaction(30,"September 2021")
+        await browser.select_to_date_for_finding_transaction(30,"September 2022")
         await browser.click_button_find()
         var list_item = [['2012-09-06','ONLINE TRANSFER REF #UKKSDRQG6L',984.3,''],['2012-09-01','ONLINE TRANSFER REF #UKKSDRQG6L',1000,'']]
         for(var i = 0;i < list_item.length;i++){
